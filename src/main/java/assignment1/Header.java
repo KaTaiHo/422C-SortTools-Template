7bb15a00b3c99bff7796812046a7793e771d81ce
Ka Tai Ho <kataihoo@gmail.com>
2019-03-30 17:44:51 -0500

    pushing starter code

---
+++ src/main/java/assignment1/Header.java
@@ -0,0 +1,9 @@
+/*
+ * EE422C Project 1 submission by
+ * Replace <...> with your actual data.
+ * <Student Name>
+ * <Student EID>
+ * <5-digit Unique No.>
+ * Spring 2019
+ * Slip days used: 
+ */
