bb95cd91be9bb2be365b92451703ba208753a508
Ka Tai Ho <kataihoo@gmail.com>
2019-03-30 17:30:49 -0500

    adding gradle build

--- Header.java
+++
@@ -1,9 +0,0 @@
-/*
- * EE422C Project 1 submission by
- * Replace <...> with your actual data.
- * <Student Name>
- * <Student EID>
- * <5-digit Unique No.>
- * Spring 2019
- * Slip days used: 
- */
